f80afc508b713d11f40e15a1fc5331b008b61ba8
Anouk Drenthe <adrenthe13@gmail.com>
2018-04-10 18:16:47 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# FrontendOpdracht2-3
+# frontend
+
+Op deze website is een API ingeladen met films. Deze films worden laten zien in een carousel waar je doorheen kunt klikken. Tevens wordt er een samenvatting van elke film laten zien en een lijst met de acteurs die erin spelen. 
+
+De carrousel kun je op verschillende manieren bedienen. Door namelijk op de pijltjes (op de pagina) te klikken en op de pijltjes op je toetsenbord. In lijst met acteurs staat foto's en namen waar je doorheen kunt scrollen met je trackpad.
+
+
+![alt text](file:///Users/Anouk/Desktop/Schermafbeelding%202018-04-10%20om%2016.36.42.png)
+![alt text](file:///Users/Anouk/Desktop/Schermafbeelding%202018-04-10%20om%2016.36.53.png)
---
+++ json_js/java.js
@@ -0,0 +1,82 @@
+ $.getJSON("json.json", function (data) {
+             for (i = 0; i < data.length; i++) {
+                 var filmnaam = data[i].title;
+                 var actor = data[i].actors;
+                 var plot = data[i].plot;
+                 var cover = data[i].cover;
+
+                 $('section').append('<article id="movie' + i + '">\
+                    <h1>Anouks Favorite movies:</h1>\
+                    <img class = "animated flip flipOutY lightSpeedOut rotateIn"\
+                     src="' + cover + '">\
+                     <h1> ' + filmnaam + ' </h1>\ <h2>Plot</h2>\
+                    <p>' + plot + '</p>\
+                    <h1>Actors</h1>\
+                    <div class = "actors scrollmenu"></div></article>')
+
+                     for (a = 0; a < actor.length; a++) {
+                         var actor_name = actor[a].actor_name;
+                         var photo = actor[a].url_photo;
+
+
+                         $('#movie' + i + ' .actors').append('<article>\
+                    <img class "profile-picuters src="' + photo + '\
+                    "><h1>' + actor_name + '\</h1></article>')
+                     }; //for: actors
+                 }; //for: data
+             }).done(function () {
+             $('section > article:nth-of-type(1)').addClass('active');
+         });
+
+         $(function () {
+             function go_next() {
+                 var current_movie = $('#main > article.active').next();
+                 var last_movie = $('#main  > article').last();
+
+                 if (last_movie.hasClass('active')) {
+                     $('#main  > article').removeClass('active');
+                     $('#main > article:nth-of-type(1)').addClass('active');
+
+                 } else {
+                     $('#main  > article').removeClass('active');
+                     $(current_movie).addClass('active');
+                 };
+             }; //for: next
+
+             function go_prev() {
+                 var current_movie = $('#main  > article.active').prev();
+                 var first_movie = $('#main  > article').first();
+
+                 if (first_movie.hasClass('active')) {
+                     $('#main  > article').removeClass('active');
+                     $('#main  > article:last-of-type').addClass('active');
+
+                 } else {
+                     $('#main > article').removeClass('active');
+                     $(current_movie).addClass('active');
+                 };
+
+             }; //for: prev
+
+
+             $('.right').on("click", function () {
+                 go_next();
+             });
+
+             $('.left').on("click", function () {
+                 go_prev();
+             });
+
+             $(document).keydown(function (e) {
+                 if (e.keyCode == 37) {
+                     go_prev();
+                 }
+             }); //for: next keyboard
+
+             $(document).keydown(function (e) {
+                 if (e.keyCode == 39) {
+                     go_next();
+                 }
+             }); //for: prev keyboard
+
+         });
